50f669ea3e5a5c67fd01654333af9b0ca5150b6b
David Chetty <d.chetty@truendo.com>
2024-10-28 10:26:45 +0200

    Creates controller

---
+++ Controllers/PublicationController.cs
@@ -0,0 +1,26 @@
+﻿using ABCPublishingChallenge.Model;
+using ABCPublishingChallenge.Service;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ABCPublishingChallenge.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PublicationController : Controller
+    {
+        private readonly IPublicationService _publicationService;
+
+        public PublicationController(IPublicationService publicationService)
+        {
+            _publicationService = publicationService;
+        }
+
+        [HttpGet]
+        public IActionResult GetWholePublication()
+        {
+            Dictionary<string, Publication> data = _publicationService.DeserializeJsonToDictionary();
+
+            return Ok(data);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,6 +1,9 @@
+using ABCPublishingChallenge.Service;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddSingleton<IPublicationService, PublicationService>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
