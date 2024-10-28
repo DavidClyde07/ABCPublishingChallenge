22926a18c26f4d10b7c9169f520520eeff4d7722
David Chetty <d.chetty@truendo.com>
2024-10-28 09:54:10 +0200

    Creates model

---
+++ Model/Navigation.cs
@@ -0,0 +1,8 @@
+﻿namespace ABCPublishingChallenge.Model
+{
+    public class Navigation
+    {
+        public string Text { get; set; }
+        public string Section { get; set; }
+    }
+}
---
+++ Model/Publication.cs
@@ -0,0 +1,12 @@
+﻿namespace ABCPublishingChallenge.Model
+{
+    public class Publication
+    {
+        public string Title { get; set; }
+        public List<string> Content { get; set; }
+        public List<Navigation> Navigation
+        {
+            get; set;
+        }
+    }
+}
